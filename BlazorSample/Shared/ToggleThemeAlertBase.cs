3ae28430e83658f4ce5819eef56d9fc85c60f71f
Adam O'Neil <adamosoftware@gmail.com>
2020-02-03 22:04:35 -0500

    Add project files.

---
+++ BlazorSample/Shared/ToggleThemeAlertBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorSample.Shared
+{
+    public class ToggleThemeAlertBase : ComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        protected bool IsDark;
+        protected string Theme => IsDark ? "dark" : "light";
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            IsDark = true;
+        }
+    }
+}
